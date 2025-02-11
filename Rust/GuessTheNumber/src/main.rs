d4d455af4f9f82dcf9a91b3552e432e5ea784dac
Adithya <adithyakamath96@gmail.com>
2025-02-12 02:48:12 +0530

    input validation

--- Rust/GuessTheNumber/src/main.rs
+++ Rust/GuessTheNumber/src/main.rs
@@ -6,6 +6,21 @@ fn main() {
     println!(" ***Guess the number");
     let secret_num = rand::thread_rng().gen_range(1..=100); //Might need to explore a new method
 
+    loop {
+        println!("Enter your guess (1..100)");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read input!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Enter a valid number");
+                continue;
+            }
+        };
+
+
+    }
 
 
 }
